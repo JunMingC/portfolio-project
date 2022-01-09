72a586001f480f8f948b5a3b8f4b9b9c5aca93bf
JunMingC <jm96.chan@gmail.com>
2022-01-10 03:55:00 +0800

    Project Setup
    Setup redux, material-ui.
    Install missing dependencies.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+import CssBaseline from '@mui/material/CssBaseline';
 
+import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,7 +13,10 @@ import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
